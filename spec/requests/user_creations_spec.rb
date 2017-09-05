612336d9378b9403ff5d4c0a3d288172c029b0b6
snayrouz <samuel.nayrouz@gmail.com>
2017-09-05 12:28:31 -0600

    Build show new and create for user

---
+++ spec/requests/user_creations_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "User" do
+  describe "User visits main page" do
+    it "a user can be created" do
+
+      visit new_user_path
+
+      fill_in "Name", with: "Sam"
+      fill_in "Email", with: "snayrouz@gmail.com"
+      fill_in "Password", with: "password"
+      click_button "Create Account"
+
+      expect(page).to have_content("Welcome, Sam")
+    end
+  end
+end
